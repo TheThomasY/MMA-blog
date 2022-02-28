2c2e65a43a7c7ee180fcfc66d5225350a0807207
TheThomasY <twy1g16@soton.ac.uk>
2022-02-28 14:36:10 +0000

    feat: basic nav content added

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <ul className='flex w-4/5 mx-auto'>
+        <li>
+          <a href=''>MMA News</a>
+        </li>
+        <li>
+          <a href=''>Home</a>
+        </li>
+        <li>
+          <a href=''>Results</a>
+        </li>
+        <li className='ml-auto'>
+          <a href=''>Search</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
