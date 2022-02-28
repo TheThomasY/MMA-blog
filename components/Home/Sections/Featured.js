6fa6c86c0b51fe13a18dae136a1ad23d8956dd61
TheThomasY <twy1g16@soton.ac.uk>
2022-02-28 15:52:07 +0000

    feat: featured section added but not 100% ideal

--- components/Home/Sections/Featured.js
+++ components/Home/Sections/Featured.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
+// * Next
+import Image from 'next/image';
+
+// * Files
+
 export default function Featured() {
-  return <div>Featured</div>;
+  return (
+    <section className='mt-14'>
+      <h1 className='text-5xl font-semibold'>Featured</h1>
+      <div className='flex gap-5 mt-6'>
+        <div className='relative h-[200px] sm:h-[355px] lg:h-[400px] w-[870px]'>
+          <Image
+            src='/covington.jpg'
+            alt='me'
+            alt='Poster for UFC 272 Covington vs Masvidal'
+            layout='fill'
+            objectFit='cover'
+            className='rounded-lg'
+          />
+        </div>
+        <p className='hidden w-72 lg:block'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum non,
+          enim saepe quaerat suscipit odio quasi maxime ipsam nesciunt dolore
+          illum sequi quos ipsa possimus incidunt eveniet ullam consectetur
+          consequuntur distinctio sit, ut omnis aut. Dolore voluptates possimus
+          iure facilis?
+        </p>
+      </div>
+    </section>
+  );
 }
--- components/Home/Sections/Latest.js
+++ components/Home/Sections/Latest.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Latest() {
-  return <div>Latest</div>;
+  return (
+    <section className='mt-14'>
+      <h2 className='text-3xl font-semibold'>Latest</h2>
+    </section>
+  );
 }
