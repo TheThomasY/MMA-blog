1703e0f0eeebd59689be8300833023e188a51bb8
TheThomasY <twy1g16@soton.ac.uk>
2022-02-28 17:37:43 +0000

    feat: mobile nav button toggle added, links dont go anywhere yet

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // * React Icons
 import { AiOutlineMenu } from 'react-icons/ai';
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(true);
+
+  const toggleNav = () => {
+    setShowNav((prevShowNav) => {
+      return !prevShowNav;
+    });
+  };
+
   return (
     <nav className='pt-8'>
       <ul className='flex items-center w-4/5 mx-auto'>
@@ -19,10 +27,17 @@ export default function Navbar() {
         <li className='hidden md:block'>
           <a href=''>Search</a>
         </li>
-        <li className='ml-auto cursor-pointer md:hidden'>
+        <li onClick={toggleNav} className='ml-auto cursor-pointer md:hidden'>
           <AiOutlineMenu size={'18px'} />
         </li>
       </ul>
+      {showNav && (
+        <ul className='w-4/5 mx-auto md:hidden'>
+          <li className='mt-4'>Home</li>
+          <li className='mt-4'>Results</li>
+          <li className='mt-4'>Search</li>
+        </ul>
+      )}
     </nav>
   );
 }
